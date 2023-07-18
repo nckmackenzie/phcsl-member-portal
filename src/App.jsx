b6bf7d40b9b81b1be340e34bba5930daba7f2b0f
nicmackenzie <nicmackenzie@live.com>
2023-07-18 16:17:20 +0300

    add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import GlobalStyles from '../GlobalStyles';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
