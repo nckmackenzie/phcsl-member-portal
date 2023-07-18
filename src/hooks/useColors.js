6192f0da609a9ddffcb5e2a42afd0bfd9d777c6a
nicmackenzie <nicmackenzie@live.com>
2023-07-18 16:16:24 +0300

    change colors for themes

--- src/hooks/useColors.js
+++ src/hooks/useColors.js
@@ -2,8 +2,8 @@ import { useTheme } from '../context/ThemeContext';
 export function useColors() {
   const { isDarkMode } = useTheme();
 
-  const textColor = isDarkMode ? 'text-light-text' : 'text-dark-text';
-  const bgColor = isDarkMode ? 'bg-light-bg' : 'bg-dark-bg';
+  const textColor = isDarkMode ? 'text-dark-text' : 'text-light-text';
+  const bgColor = isDarkMode ? 'bg-dark-bg' : 'bg-light-bg';
 
   return { textColor, bgColor };
 }
