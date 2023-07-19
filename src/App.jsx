b1c3f3b4cc63e727000cf74c90619986511c0ea8
nicmackenzie <nicmackenzie@live.com>
2023-07-19 12:30:37 +0300

    add react-query devtools component and Toaster component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
+
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import GlobalStyles from '../GlobalStyles';
@@ -11,12 +14,33 @@ function App() {
     <>
       <GlobalStyles />
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <BrowserRouter>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Dashboard />} />
           </Routes>
         </BrowserRouter>
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: '8px' }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              maxWidth: '500px',
+              fontSize: '16px',
+              padding: '16px 24px',
+              backgroundColor: 'var(--color-grey-0)',
+              color: 'var(--color-grey-700)',
+            },
+          }}
+        />
       </QueryClientProvider>
     </>
   );
