12eec6cca72da19903fba1f13e75980daf29a061
nicmackenzie <nicmackenzie@live.com>
2023-07-19 22:10:03 +0300

    Reusable button icon component

---
+++ src/ui/ButtonIcon.jsx
@@ -0,0 +1,21 @@
+import { styled } from 'styled-components';
+
+const ButtonIcon = styled.button`
+  background: none;
+  border: none;
+  padding: 0.375rem;
+  border-radius: var(--border-radius-sm);
+  transition: all 0.2s;
+
+  &:hover {
+    background-color: var(--color-grey-100);
+  }
+
+  & svg {
+    width: 1.375rem;
+    height: 1.375rem;
+    color: var(--color-${props => props.color});
+  }
+`;
+
+export default ButtonIcon;
