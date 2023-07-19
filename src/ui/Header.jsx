de873b97047a1690d89b93c259a0c7ea4898d308
nicmackenzie <nicmackenzie@live.com>
2023-07-19 22:37:42 +0300

    Header component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,20 +1,38 @@
-import styled from 'styled-components';
+import { styled } from 'styled-components';
+import { HiOutlineMenu } from 'react-icons/hi';
 import UserMenu from './UserMenu';
+import ButtonIcon from './ButtonIcon';
+import { useSidebar } from '../context/SidebarContext';
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
-  padding: 0.75rem 4rem;
+  padding: 0.75rem 1rem;
   border-bottom: 1px solid var(--color-grey-100);
 
   display: flex;
   gap: 1.5rem;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
+  height: 6rem;
+
+  @media (min-width: 1024px) {
+    padding: 0.75rem 4rem;
+    justify-content: flex-end;
+  }
 `;
 
 function Header() {
+  const { open } = useSidebar();
+
   return (
     <StyledHeader>
+      <ButtonIcon
+        color="grey-700"
+        className="inline-flex lg:hidden"
+        onClick={open}
+      >
+        <HiOutlineMenu />
+      </ButtonIcon>
       <UserMenu />
     </StyledHeader>
   );
