2e0b37c14aa3c152a34ffc705f4285ae6527d45e
nicmackenzie <nicmackenzie@live.com>
2023-07-20 11:43:08 +0300

    App Navigation

---
+++ src/ui/MainNav.jsx
@@ -0,0 +1,90 @@
+import {
+  HiBanknotes,
+  HiOutlineHome,
+  HiOutlineUser,
+  HiOutlineHomeModern,
+} from 'react-icons/hi2';
+import { NavLink } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+const StyledMainNav = styled.nav``;
+
+const NavItems = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
+
+const StyledLink = styled(NavLink)`
+  &:link,
+  &:visited {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    color: var(--color-grey-600);
+    font-size: 1rem;
+    font-weight: 500;
+    padding: 0.75rem 1.5rem;
+    transition: all 0.3s;
+  }
+
+  /* This works because react-router places the active class on the active NavLink */
+  &:hover,
+  &:active,
+  &.active:link,
+  &.active:visited {
+    color: var(--color-grey-800);
+    background-color: var(--color-grey-50);
+    border-radius: var(--border-radius-sm);
+  }
+
+  & svg {
+    width: 1.5rem;
+    height: 1.5rem;
+    color: var(--color-grey-400);
+    transition: all 0.3s;
+  }
+
+  &:hover svg,
+  &:active svg,
+  &.active:link svg,
+  &.active:visited svg {
+    color: var(--color-brand-600);
+  }
+`;
+
+function MainNav() {
+  return (
+    <StyledMainNav>
+      <NavItems>
+        <li>
+          <StyledLink to="/">
+            <HiOutlineHome />
+            <span>Dashboard</span>
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/payments">
+            <HiBanknotes />
+            <span>Payments</span>
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/available-units">
+            <HiOutlineHomeModern />
+            <span>Available Units</span>
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/my-profile">
+            <HiOutlineUser />
+            <span>My Profile</span>
+          </StyledLink>
+        </li>
+      </NavItems>
+    </StyledMainNav>
+  );
+}
+
+export default MainNav;
