02e249bce151458473b8b0b799c1a13d5b6bab2d
nicmackenzie <nicmackenzie@live.com>
2023-07-20 14:03:29 +0300

    state for active tab

--- src/features/profile/ProfileBox.jsx
+++ src/features/profile/ProfileBox.jsx
@@ -1,24 +1,77 @@
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
+import { BsFileEarmarkPdf } from 'react-icons/bs';
 import Tabs from './Tabs';
+import { useState } from 'react';
 
 const StyledProfileBox = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 0.8rem;
+  gap: 0.25rem;
 `;
 
 const Actions = styled.div`
   display: flex;
+  flex-direction: column-reverse;
+  gap: 0.8rem;
+
+  @media (min-width: 640px) {
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    gap: 0;
+  }
+`;
+
+const DownloadButton = styled.button`
+  box-shadow: var(--shadow-sm);
+  border-radius: var(--border-radius-sm);
+  padding: 0.25rem 0.5rem;
+  border: none;
+  background-color: var(--color-brand-400);
+  color: var(--color-brand-50);
+  font-weight: 500;
+  font-size: 0.875rem;
+  display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
+  gap: 0.5rem;
+  align-self: stretch;
+
+  @media (min-width: 640px) {
+    justify-content: flex-start;
+  }
+`;
+
+const FormArea = styled.div`
+  ${props =>
+    props.type === 'My Details' &&
+    css`
+      max-width: 56rem;
+    `}
+  ${props =>
+    props.type === 'Security' &&
+    css`
+      max-width: 32rem;
+    `}
+
+    border: 1px solid var(--color-grey-100);
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+  border-radius: var(--border-radius-sm);
+  padding: 0.25rem;
 `;
 
 function ProfileBox() {
+  const [activeTab, setActiveTab] = useState('My Details');
   return (
     <StyledProfileBox>
       <Actions>
-        <Tabs />
+        <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        <DownloadButton>
+          <BsFileEarmarkPdf /> <span>Download Statement</span>
+        </DownloadButton>
       </Actions>
+      <FormArea type={activeTab}>form area</FormArea>
     </StyledProfileBox>
   );
 }
