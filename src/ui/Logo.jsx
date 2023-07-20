0537362ded38bf3d289097c62eebe7d76cbec317
nicmackenzie <nicmackenzie@live.com>
2023-07-20 12:13:32 +0300

    add close button

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,20 +1,41 @@
 import { styled } from 'styled-components';
+import ButtonIcon from './ButtonIcon';
+import { HiOutlineXMark } from 'react-icons/hi2';
+import { useSidebar } from '../context/SidebarContext';
 
 const StyledLogo = styled.div`
   text-align: center;
 `;
 
 const Img = styled.img`
-  height: 6rem;
+  height: 4.5rem;
   width: auto;
+
+  @media (min-width: 768px) {
+    height: 6rem;
+  }
 `;
 
-function Logo() {
-  return (
-    <StyledLogo>
-      <Img src="/logos/logo_landscape.png" alt="Logo" />
-    </StyledLogo>
-  );
+function Logo({ logoOnly = true }) {
+  const { close } = useSidebar();
+  if (logoOnly)
+    return (
+      <StyledLogo>
+        <Img src="/logos/logo_landscape.png" alt="Logo" />
+      </StyledLogo>
+    );
+
+  if (!logoOnly)
+    return (
+      <div className="flex flex-col gap-1">
+        <ButtonIcon className="self-end" onClick={close}>
+          <HiOutlineXMark />
+        </ButtonIcon>
+        <StyledLogo>
+          <Img src="/logos/logo_landscape.png" alt="Logo" />
+        </StyledLogo>
+      </div>
+    );
 }
 
 export default Logo;
