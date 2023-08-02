bca124458aeb12a85cb8880667362b83f8f6fb7c
nicmackenzie <nicmackenzie@live.com>
2023-08-02 09:37:17 +0300

    add align items side by side

--- src/ui/LoadingButtonText.jsx
+++ src/ui/LoadingButtonText.jsx
@@ -1,11 +1,18 @@
+import { styled } from 'styled-components';
 import Spinner from './Spinner';
+
+const StyledButtonText = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
 
 function LoadingButtonText({ text }) {
   return (
-    <>
+    <StyledButtonText>
       <Spinner size="small" />
       <span>{text}</span>
-    </>
+    </StyledButtonText>
   );
 }
 
