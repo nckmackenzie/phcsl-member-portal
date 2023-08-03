1393a6a22d601796a5e5ed4893ea4c0b909b6c86
nicmackenzie <nicmackenzie@live.com>
2023-08-03 14:03:58 +0300

    add function to return first chars as upper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,3 +2,7 @@ export const formatCurrency = value =>
   new Intl.NumberFormat('en', { style: 'currency', currency: 'Ksh' }).format(
     value
   );
+
+export function capitalizeEveryWord(str) {
+  return str.replace(/\b\w/g, char => char.toUpperCase());
+}
